09921d1796a310b1186b12829703e8ebe4679c34
Elijah Wayne <eli.afedzie@gmail.com>
2025-08-31 19:13:44 -0400

    teams page

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
+teams = {} #teams db current
+current_id = 1
+
 @app.route('/')
 def home():
-    return 'Cargill default home page for TH interview'
+    return 'Cargill Teams App Home page' 
+
+@app.route('/teams', methods=['GET']) #Get Teams
+def get_teams():
+    return jsonify(list(teams.values()))
 
 if __name__ == '__main__':
     app.run(debug=True)
